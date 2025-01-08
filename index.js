65ac8dd8fe5fa93d3f830be147e288f64546d49f
Thisbe Scholfield-Johnson <ts1299@georgetown.edu>
2025-01-07 19:28:56 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'Thisbe'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Ted'
+}
+// console.log(customerName);
+// console.log(customerName.toUpperCase())
